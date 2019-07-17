cb133db08c971b528ff347a21d864ac0eb29a235
amarek1 <31806815+amarek1@users.noreply.github.com>
2019-07-17 16:38:04 +0100

    Update forest_code.py

--- 1) classification algorithms/random forest/credit card fraud/forest_code.py
+++ 1) classification algorithms/random forest/credit card fraud/forest_code.py
@@ -8,7 +8,7 @@ from global_functions import get_balanced_data
 np.random.seed(7)
 
 
-def get_forest_model(dataset_name='data_creditcard.pkl', balanced=False):
+def get_forest_model(dataset_name='data_creditcard.pkl', balanced=False, model_name='model_forest_unbalanced.pkl'):
     # load the data
     file_name = 'data/credit card fraud/'+dataset_name  # set working directory to MSc Project
     data = pd.read_pickle(file_name)
@@ -20,13 +20,6 @@ def get_forest_model(dataset_name='data_creditcard.pkl', balanced=False):
         y = data['class']
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
-    return X_train, X_test, y_train, y_test
-
-X_train, X_test, y_train, y_test = get_forest_model(balanced=True)
-
-
-# perform grid search to find the best parameters
-def get_best_params(train_data, train_labels):
     # Number of trees in random forest
     n_estimators = [10, 100]  # [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
     # Number of features to consider at every split
@@ -56,22 +49,16 @@ def get_best_params(train_data, train_labels):
     # Random search of parameters, using 3 fold cross validation,
     # search across 100 different combinations, and use all available cores
     rf_random = GridSearchCV(estimator=rf, param_grid=random_grid, cv=3, verbose=2, n_jobs=1)
-    rf_random.fit(train_data, train_labels)
-    print(rf_random.best_params_)
+    rf_random.fit(X_train, y_train)
+    clf = rf_random.best_estimator_
 
-best_params = get_best_params(X_train[0:50], y_train[0:50])
-print(best_params[1])
+    model = clf.fit(X_train, y_train)
+
+    path = '1) classification algorithms/random forest/credit card fraud/' + model_name
+    with open(path, 'wb') as file:
+        pickle.dump(model, file)
+
+    return
 
 
-# optimized_model = RandomForestRegressor(bootstrap=True, max_depth=5, max_features='auto',
-#                                          min_samples_split=2, n_estimators=100)
-#
-# # Fit the random search model
-# model = optimized_model.fit(X_train, y_train)
-#
-# # path for saving the model
-# path = 'C:/Users/amarek/PycharmProjects/data_lab/models/decision tree/forest_model_bal_ori.pkl'
-#
-# # save the model
-# with open(path, 'wb') as file:
-#     pickle.dump(model, file)
+get_forest_model()
