c35b56062c5c45cada711187f01adfb8982ce959
MAREK Anna <amarek@ed.ac.uk>
2019-07-25 09:10:28 +0100

    Update recall_improvement_evaluation.py

--- 4) final figures/recall improvement/recall_improvement_evaluation.py
+++ 4) final figures/recall improvement/recall_improvement_evaluation.py
@@ -82,12 +82,44 @@ X_train_balanced, X_test_balanced, y_train_balanced, y_test_balanced = get_balan
 
 ############# synthetic on top of original fraud #############################
 
+# def plot_this(score='f1-score'):
+#     fraud_data_size = [0,100,200,300,400,500,1000,2000,3000,4000,5000]
+#     d={}
+#     report_dict_mix={}
+#     for i in range(0, len(fraud_data_size)):
+#         path = '4) final figures/recall improvement/models/' + 'ori and syn fraud/model_forest_unbalanced_mix_fraud_' + str(
+#             fraud_data_size[i]) + '.pkl'
+#         with open(path, 'rb') as file:
+#             d['model_'+str(fraud_data_size[i])] = pickle.load(file)
+#             model_predictions = d['model_'+str(fraud_data_size[i])].predict(X_test_balanced)
+#             model_predictions = [int(round(x)) for x in model_predictions]
+#             report = classification_report(y_test_balanced, model_predictions, labels=None,
+#                                            target_names=['normal', 'fraud'], digits=2, output_dict=True)
+#             report_dict_mix['model_' + str(fraud_data_size[i])]=report
+#
+#
+#     plt.plot([0,100,200,300,400,500,1000,2000,3000,4000,5000],[report_dict_mix['model_0']['fraud'][score],report_dict_mix['model_100']['fraud'][score],
+#                           report_dict_mix['model_200']['fraud'][score],report_dict_mix['model_300']['fraud'][score],
+#                                   report_dict_mix['model_400']['fraud'][score],
+#                                       report_dict_mix['model_500']['fraud'][score],report_dict_mix['model_1000']['fraud'][score],report_dict_mix['model_2000']['fraud'][score],
+#                           report_dict_mix['model_3000']['fraud'][score],report_dict_mix['model_4000']['fraud'][score],
+#                                   report_dict_mix['model_5000']['fraud'][score]],marker='o')
+#     plt.axhline(y=report_dict_mix['model_0']['fraud'][score], color='r', linestyle='--')
+#     plt.ylabel(score)
+#     plt.xlabel('# synthetic fraud data')
+#     plt.title('The effect of adding synthetic data to original training data on '+str(score))
+#     #plt.yticks(np.arange(0.7, 0.801, 0.01))
+#     plt.grid()
+#     plt.savefig('4) final figures/recall improvement/effect of adding fraud data on '+str(score)+'2.png')
+#     plt.show()
+#     return
+
 def plot_this(score='f1-score'):
     fraud_data_size = [0,100,200,300,400,500,1000,2000,3000,4000,5000]
     d={}
     report_dict_mix={}
     for i in range(0, len(fraud_data_size)):
-        path = '4) final figures/recall improvement/models/' + 'ori and syn fraud/model_forest_unbalanced_ori_and_syn_fraud_' + str(
+        path = '4) final figures/recall improvement/models/' + 'ori and syn fraud/model_forest_balanced_mix_fraud_' + str(
             fraud_data_size[i]) + '.pkl'
         with open(path, 'rb') as file:
             d['model_'+str(fraud_data_size[i])] = pickle.load(file)
@@ -103,15 +135,14 @@ def plot_this(score='f1-score'):
                                   report_dict_mix['model_400']['fraud'][score],
                                       report_dict_mix['model_500']['fraud'][score],report_dict_mix['model_1000']['fraud'][score],report_dict_mix['model_2000']['fraud'][score],
                           report_dict_mix['model_3000']['fraud'][score],report_dict_mix['model_4000']['fraud'][score],
-                                  report_dict_mix['model_5000']['fraud'][score],
-                                      report_dict_mix['model_500']['fraud'][score]],marker='o')
+                                  report_dict_mix['model_5000']['fraud'][score]],marker='o')
     plt.axhline(y=report_dict_mix['model_0']['fraud'][score], color='r', linestyle='--')
     plt.ylabel(score)
     plt.xlabel('# synthetic fraud data')
     plt.title('The effect of adding synthetic data to original training data on '+str(score))
-    plt.yticks(np.arange(0.7, 0.801, 0.01))
+    #plt.yticks(np.arange(0.7, 0.801, 0.01))
     plt.grid()
-    plt.savefig('4) final figures/recall improvement/effect of adding fraud data on del'+str(score)+'.png')
+    plt.savefig('4) final figures/recall improvement/balanced effect of adding fraud data on '+str(score)+'2.png')
     plt.show()
     return
 
