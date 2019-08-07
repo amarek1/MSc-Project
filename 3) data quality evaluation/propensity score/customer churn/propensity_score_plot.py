f5490c9c8e60089e7d31bdc3a6ba875a916a5373
MAREK Anna <amarek@ed.ac.uk>
2019-08-07 13:43:51 +0100

    Create propensity_score_plot.py

---
+++ 3) data quality evaluation/propensity score/customer churn/propensity_score_plot.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+np.random.seed(7)
+
+score_synthpop = 0.19
+score_GAN = 0.245
+score_cGAN = 0.235
+score_WGAN = 0.19
+score_WcGAN = 0.17
+score_tGAN = 0.23
+
+
+fig = plt.figure()
+x = np.arange(6)
+distance = [score_synthpop, score_GAN, score_cGAN, score_WGAN, score_WcGAN, score_tGAN]
+plt.title('Synthetic ranking agreement results')
+plt.ylabel('Score')
+plt.xlabel('data generator')
+a = plt.bar(x, distance)
+plt.xticks(x, ('synthpop', 'GAN', 'cGAN', 'WGAN', 'WcGAN', 'tGAN'))
+plt.tight_layout()
+a[0].set_color('lightseagreen')
+a[1].set_color('gold')
+a[2].set_color('darkorange')
+a[3].set_color('grey')
+a[4].set_color('dodgerblue')
+a[5].set_color('mediumvioletred')
+plt.savefig('3) data quality evaluation/propensity score/credit card fraud/propensity_score_plot.png')
