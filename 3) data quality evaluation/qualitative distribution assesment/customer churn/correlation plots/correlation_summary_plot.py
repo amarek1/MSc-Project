36d3ff5b196b401b0b1d471c35353655a82e017d
MAREK Anna <amarek@ed.ac.uk>
2019-08-08 15:12:16 +0100

    Create correlation_summary_plot.py

---
+++ 3) data quality evaluation/qualitative distribution assesment/customer churn/correlation plots/correlation_summary_plot.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+np.random.seed(7)
+
+score_synthpop = 0.02127
+score_GAN = 0.25511
+score_cGAN = 0.4
+score_WGAN = 0.02423
+score_WcGAN = 0.24698
+score_tGAN = 0.0458
+
+
+fig = plt.figure()
+x = np.arange(6)
+distance = [score_synthpop, score_GAN, score_cGAN, score_WGAN, score_WcGAN, score_tGAN]
+plt.title('Correlation mean difference for churn dataset')
+plt.ylabel('Score')
+plt.xlabel('data generator')
+a = plt.bar(x, distance)
+plt.xticks(x, ('synthpop', 'GAN', 'cGAN', 'WGAN', 'WcGAN', 'tGAN'))
+plt.tight_layout()
+a[0].set_color('lightseagreen')
+a[1].set_color('gold')
+a[2].set_color('darkorange')
+a[3].set_color('grey')
+a[4].set_color('dodgerblue')
+a[5].set_color('mediumvioletred')
+plt.savefig('3) data quality evaluation/qualitative distribution assesment/customer churn/correlation plots/correlation_summary_churn.png')
