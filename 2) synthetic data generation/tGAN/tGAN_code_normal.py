bc9efb5a0fd372cb9d629fe524609e8798b6929f
MAREK Anna <amarek@ed.ac.uk>
2019-08-08 15:12:33 +0100

    Update tGAN_code_normal.py

--- 2) synthetic data generation/tGAN/tGAN_code_normal.py
+++ 2) synthetic data generation/tGAN/tGAN_code_normal.py
@@ -9,9 +9,9 @@ file_name = 'data/customer churn/customer churn modified.pkl'
 ori_data = pd.read_pickle(file_name)
 fraud_data = ori_data.loc[ori_data['class'] == 0]
 
-continuous_columns = [*range(0, len(ori_data.columns)-1)]
+continuous_columns = [3,6,7]
 
-tgan = TGANModel(continuous_columns, output='2) synthetic data generation/tGAN/customer churn/normal/', max_epoch=1, steps_per_epoch=6000, save_checkpoints=True,
+tgan = TGANModel(continuous_columns, output='2) synthetic data generation/tGAN/customer churn/normal/', max_epoch=1, steps_per_epoch=2000, save_checkpoints=True,
                  restore_session=True, batch_size=256, z_dim=200, noise=0.2, l2norm=0.00001, learning_rate=0.001,
                  num_gen_rnn=100, num_gen_feature=100, num_dis_layers=1, num_dis_hidden=100, optimizer='AdamOptimizer')
 
