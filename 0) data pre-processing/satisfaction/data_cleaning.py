9aeed0457b1c8b85f06900ac8bb8bd0a46045083
amarek1 <31806815+amarek1@users.noreply.github.com>
2019-08-19 10:26:48 +0100

    Update data_cleaning.py

--- 0) data pre-processing/satisfaction/data_cleaning.py
+++ 0) data pre-processing/satisfaction/data_cleaning.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import RobustScaler
 
 file_name = 'data/satisfaction/satisfaction.csv'
 data = pd.read_csv(file_name)
 
 stats = data.describe()
 # stats.to_csv('data/satisfaction/stats.csv')  # easier to quickly view in excel
+
+# check for NANs - no NaNs
+data.columns[data.isna().any()].tolist()
 
 # check number of 0 and 1 class
 print(data['TARGET'].value_counts())
@@ -35,8 +39,7 @@ remove_dup += ['num_var13_medio', 'saldo_medio_var13_medio_ult1']
 
 data.drop(remove_dup, axis=1, inplace=True)
 
-# check for NANs - no NaNs
-data.columns[data.isna().any()].tolist()
+
 
 
 # determine binary/categorical columns (0 or 1)
@@ -48,12 +51,27 @@ for col in data.columns:
 # rename TARGET to class for consistency
 data = data.rename(columns={'TARGET':'class'})
 
-data.to_pickle('data/satisfaction/satisfaction clean.pkl')
-data.to_csv('data/satisfaction/satisfaction clean.csv')
+# rescale continous data to be between -1 and 1
+continous_cols = [x for x in data.columns if x not in ['ind_var1_0', 'ind_var1', 'ind_var5_0', 'ind_var5', 'ind_var6_0', 'ind_var6', 'ind_var8_0', 'ind_var8',
+        'ind_var12_0', 'ind_var12', 'ind_var13_0', 'ind_var13_corto_0', 'ind_var13_corto', 'ind_var13_largo_0',
+        'ind_var13_largo', 'ind_var13_medio_0', 'ind_var13', 'ind_var14_0', 'ind_var14', 'ind_var17_0', 'ind_var17',
+        'ind_var18_0', 'ind_var19', 'ind_var20_0', 'ind_var20', 'ind_var24_0', 'ind_var24', 'ind_var25_cte',
+        'ind_var26_0', 'ind_var26_cte', 'ind_var25_0', 'ind_var30_0', 'ind_var30', 'ind_var31_0', 'ind_var31',
+        'ind_var32_cte', 'ind_var32_0', 'ind_var33_0', 'ind_var33', 'ind_var34_0', 'ind_var37_cte', 'ind_var37_0',
+        'ind_var39_0', 'ind_var40_0', 'ind_var40', 'ind_var41_0', 'ind_var44_0', 'ind_var44', 'ind_var7_emit_ult1',
+        'ind_var7_recib_ult1', 'ind_var10_ult1', 'ind_var10cte_ult1', 'ind_var9_cte_ult1', 'ind_var9_ult1',
+        'ind_var43_emit_ult1', 'ind_var43_recib_ult1', 'class']]
+
+rob_scaler = RobustScaler()
+for i in continous_cols:
+    data[i] = rob_scaler.fit_transform(data[i].values.reshape(-1,1))
+
+data.to_pickle('data/satisfaction/satisfaction clean_scaled.pkl')
+data.to_csv('data/satisfaction/satisfaction clean_scaled.csv')
 
 # seperate data into satisfied (0) and not satisfied (1)
 normal_data = data[data['class']==0]
-normal_data.to_csv('data/satisfaction/satisfaction normal.csv')
+normal_data.to_csv('data/satisfaction/satisfaction normal_scaled.csv')
 
 notsat_data = data[data['class']==1]
-notsat_data.to_csv('data/satisfaction/satisfaction notsat.csv')
+notsat_data.to_csv('data/satisfaction/satisfaction notsat_scaled.csv')
