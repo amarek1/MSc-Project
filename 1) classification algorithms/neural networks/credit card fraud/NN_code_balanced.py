55172bb95acbe09b6302ced582ea5c52b41dfca0
amarek1 <31806815+amarek1@users.noreply.github.com>
2019-08-20 15:16:54 +0100

    Update NN_code_balanced.py

--- 1) classification algorithms/neural networks/credit card fraud/NN_code_balanced.py
+++ 1) classification algorithms/neural networks/credit card fraud/NN_code_balanced.py
@@ -6,9 +6,38 @@ from sklearn.model_selection import GridSearchCV
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
-from global_functions import get_balanced_data
+# from global_functions import get_balanced_data
 import numpy
 numpy.random.seed(7)
+
+# create train and test data with equal number of classes
+def get_balanced_data(data):
+    from sklearn.model_selection import train_test_split
+    train_data, test_data, train_labels, test_labels = \
+        train_test_split(data, data['class'], test_size=0.25, random_state=1)
+
+    # even out the data set -> 1:1 ratio of 0 and 1 classes
+    data_training = train_data.sample(frac=1)  # shuffle
+    data_testing = test_data.sample(frac=1) # shuffle
+
+    fraud_data_training = data_training.loc[data_training['class'] == 1]
+    fraud_data_testing = data_testing.loc[data_testing['class'] == 1]
+
+    non_fraud_data_training = data_training.loc[data_training['class'] == 0][:len(fraud_data_training)]
+    non_fraud_data_testing = data_testing.loc[data_testing['class'] == 0][:len(fraud_data_testing)]
+
+    even_data_training = pd.concat([fraud_data_training, non_fraud_data_training])
+    even_data_testing = pd.concat([fraud_data_testing, non_fraud_data_testing])
+
+    even_data_training = even_data_training.sample(frac=1, random_state=42)
+    even_data_testing = even_data_testing.sample(frac=1, random_state=42)
+
+    train_data = even_data_training.drop('class', axis=1)
+    test_data = even_data_testing.drop('class', axis=1)
+    train_labels = even_data_training['class']
+    test_labels = even_data_testing['class']
+
+    return train_data, test_data, train_labels, test_labels
 
 # load the data
 file_name = 'data/credit card fraud/data_creditcard.pkl'  # set working directory to MSc Project
