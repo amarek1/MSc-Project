aa4226eda14691edbc2d01aa749c568e5e911c80
amarek1 <31806815+amarek1@users.noreply.github.com>
2019-08-22 12:15:00 +0100

    Update forest_code_unbalanced.py

--- 1) classification algorithms/random forest/bioresponse/forest_code_unbalanced.py
+++ 1) classification algorithms/random forest/bioresponse/forest_code_unbalanced.py
@@ -4,9 +4,35 @@ import pickle
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
-from global_functions import get_balanced_data
+#from global_functions import get_balanced_data
 np.random.seed(7)
+def get_balanced_data(data):
 
+    train_data, test_data, train_labels, test_labels = \
+        train_test_split(data, data['class'], test_size=0.25, random_state=1)
+
+    # even out the data set -> 1:1 ratio of 0 and 1 classes
+    data_training = train_data.sample(frac=1)  # shuffle
+    data_testing = test_data.sample(frac=1) # shuffle
+
+    fraud_data_training = data_training.loc[data_training['class'] == 1]
+    fraud_data_testing = data_testing.loc[data_testing['class'] == 1]
+
+    non_fraud_data_training = data_training.loc[data_training['class'] == 0][:len(fraud_data_training)]
+    non_fraud_data_testing = data_testing.loc[data_testing['class'] == 0][:len(fraud_data_testing)]
+
+    even_data_training = pd.concat([fraud_data_training, non_fraud_data_training])
+    even_data_testing = pd.concat([fraud_data_testing, non_fraud_data_testing])
+
+    even_data_training = even_data_training.sample(frac=1, random_state=42)
+    even_data_testing = even_data_testing.sample(frac=1, random_state=42)
+
+    train_data = even_data_training.drop('class', axis=1)
+    test_data = even_data_testing.drop('class', axis=1)
+    train_labels = even_data_training['class']
+    test_labels = even_data_testing['class']
+
+    return train_data, test_data, train_labels, test_labels
 # load the data
 file_name = 'data/bioresponse/bio_clean.pkl'   # set working directory to MSc Project
 ori_data = pd.read_pickle(file_name)
