d4a22d55afef42dde4ef9a9c8cd9d2e643a709d6
amarek1 <31806815+amarek1@users.noreply.github.com>
2019-08-22 13:21:43 +0100

    Update forest_code_balanced.py

--- 1) classification algorithms/random forest/bioresponse/forest_code_balanced.py
+++ 1) classification algorithms/random forest/bioresponse/forest_code_balanced.py
@@ -4,36 +4,8 @@ import pickle
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
-#from global_functions import get_balanced_data
+from global_functions import get_balanced_data
 np.random.seed(7)
-
-def get_balanced_data(data):
-
-    train_data, test_data, train_labels, test_labels = \
-        train_test_split(data, data['class'], test_size=0.25, random_state=1)
-
-    # even out the data set -> 1:1 ratio of 0 and 1 classes
-    data_training = train_data.sample(frac=1)  # shuffle
-    data_testing = test_data.sample(frac=1) # shuffle
-
-    fraud_data_training = data_training.loc[data_training['class'] == 1]
-    fraud_data_testing = data_testing.loc[data_testing['class'] == 1]
-
-    non_fraud_data_training = data_training.loc[data_training['class'] == 0][:len(fraud_data_training)]
-    non_fraud_data_testing = data_testing.loc[data_testing['class'] == 0][:len(fraud_data_testing)]
-
-    even_data_training = pd.concat([fraud_data_training, non_fraud_data_training])
-    even_data_testing = pd.concat([fraud_data_testing, non_fraud_data_testing])
-
-    even_data_training = even_data_training.sample(frac=1, random_state=42)
-    even_data_testing = even_data_testing.sample(frac=1, random_state=42)
-
-    train_data = even_data_training.drop('class', axis=1)
-    test_data = even_data_testing.drop('class', axis=1)
-    train_labels = even_data_training['class']
-    test_labels = even_data_testing['class']
-
-    return train_data, test_data, train_labels, test_labels
 
 # load the data
 file_name = 'data/bioresponse/bio_clean.pkl'   # set working directory to MSc Project
@@ -54,7 +26,7 @@ data = ori_data
 
 # for balanced model: {'bootstrap': True, 'max_depth': 5, 'min_samples_split': 10, 'n_estimators': 100}
 # for unbalanced model:{'bootstrap': True, 'max_depth': 5, 'min_samples_split': 10, 'n_estimators': 100}
-def get_forest_model(data=data, balanced=True, model_name='model_forest_balanced_bio_vm_test.pkl'):
+def get_forest_model(data=data, balanced=True, model_name='model_forest_balanced_bio.pkl'):
 
     if balanced == True:
         X_train, X_test, y_train, y_test = get_balanced_data(data)
@@ -64,11 +36,11 @@ def get_forest_model(data=data, balanced=True, model_name='model_forest_balanced
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)
 
     # Number of trees in random forest
-    n_estimators = [10]#, 50, 100]  # [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+    n_estimators = [10, 50, 100]  # [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
     # Number of features to consider at every split
     #max_features = ['auto']  # ['auto', 'sqrt']
     # Maximum number of levels in tree
-    max_depth = [3]#, 5, 10, 25]  # [int(x) for x in np.linspace(10, 110, num = 11)]
+    max_depth = [3, 5, 10, 25]  # [int(x) for x in np.linspace(10, 110, num = 11)]
     # Minimum number of samples required to split a node
     min_samples_split = [2, 10, 20]
     #criterion = ['gini', 'entropy']
