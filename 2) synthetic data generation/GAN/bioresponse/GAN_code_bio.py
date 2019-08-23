67825fc9a04ff9f720d3c8de3693495817b4b4b6
amarek1 <31806815+amarek1@users.noreply.github.com>
2019-08-23 11:14:07 +0100

    Update GAN_code_bio.py

--- 2) synthetic data generation/GAN/bioresponse/GAN_code_bio.py
+++ 2) synthetic data generation/GAN/bioresponse/GAN_code_bio.py
@@ -20,7 +20,7 @@ data = churn_data
 def GAN_generate_data(data=data, rand_dim=32, base_n_count=128, nb_steps=6000 + 1, batch_size=128, k_d=1, k_g=1,
                       log_interval=100, learning_rate=5e-5,critic_pre_train_steps=100,
                       data_dir='2) synthetic data generation/GAN/bioresponse/GAN training/sat_1_',
-                      gen_data_size=2000, gen_data_name='GAN_bio_2000'):
+                      gen_data_size=2000, gen_data_name='GAN_bio_1_2000'):
 
 
     # set up
