412a6d2fa5864d6c7ab19db8f98b2368cd97b61b
amarek1 <31806815+amarek1@users.noreply.github.com>
2019-08-24 13:00:29 +0100

    Create bio_less_features.py

---
+++ 2) synthetic data generation/tGAN/bioresponse/0/bio_less_features.py
@@ -0,0 +1,40 @@
+import pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def unpack_model(model_name):
+    path = '1) classification algorithms/random forest/bioresponse/' + model_name
+    with open(path, 'rb') as file:
+        model = pickle.load(file)
+        return model
+
+real_model = unpack_model('model_forest_unbalanced_bio.pkl')
+
+
+# load the original data
+file_name = 'data/bioresponse/bio_clean.pkl'
+real_data = pd.read_pickle(file_name)
+
+# unbalanced data
+X = real_data.drop('class', axis=1)
+y = real_data['class']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=1)
+
+
+def get_feature_importance(model, x_train):
+    feature_importance = pd.DataFrame(model.feature_importances_, index=x_train.columns,
+                                      columns=['importance']).sort_values('importance', ascending=False)
+    return feature_importance
+
+FI = get_feature_importance(real_model, X_train)
+
+row_list = list(FI.index.values)
+
+cols_to_keep = row_list[:262]
+cols_to_keep.append('class')
+
+data = real_data[cols_to_keep]
+print(data)
+
+data.to_pickle('data/bioresponse/bio_short.pkl')
