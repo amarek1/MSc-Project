59513b65c8f655f8bca7bdf5dda270d0713d34c5
amarek1 <31806815+amarek1@users.noreply.github.com>
2019-08-24 16:49:03 +0100

    Update tGAN_code_normal_bio.py

--- 2) synthetic data generation/tGAN/bioresponse/tGAN_code_normal_bio.py
+++ 2) synthetic data generation/tGAN/bioresponse/tGAN_code_normal_bio.py
@@ -17,9 +17,9 @@ for col in ori_data.columns:
 
 continuous_columns = continuous
 
-tgan = TGANModel(continuous_columns, output='2) synthetic data generation/tGAN/bioresponse/0/', max_epoch=1, steps_per_epoch=6000, save_checkpoints=True,
+tgan = TGANModel(continuous_columns=continuous, output='2) synthetic data generation/tGAN/bioresponse/0/', gpu=0, max_epoch=1, steps_per_epoch=6000, save_checkpoints=True,
                  restore_session=True, batch_size=256, z_dim=200, noise=0.2, l2norm=0.00001, learning_rate=0.001,
-                 num_gen_rnn=100, num_gen_feature=100, num_dis_layers=1, num_dis_hidden=100, optimizer='AdamOptimizer', gpu='NVIDIA Tesla K80')
+                 num_gen_rnn=100, num_gen_feature=100, num_dis_layers=1, num_dis_hidden=100, optimizer='AdamOptimizer')
 
 tgan.fit(fraud_data)
 model_path = '2) synthetic data generation/tGAN/bioresponse/0/tGAN_bio_0_model.pkl'
