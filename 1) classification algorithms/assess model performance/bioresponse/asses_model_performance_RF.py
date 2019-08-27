3feb026bbdd0999b49284a450dac2084f252465b
amarek1 <31806815+amarek1@users.noreply.github.com>
2019-08-27 15:09:27 +0100

    Update asses_model_performance_RF.py

--- 1) classification algorithms/assess model performance/bioresponse/asses_model_performance_RF.py
+++ 1) classification algorithms/assess model performance/bioresponse/asses_model_performance_RF.py
@@ -12,7 +12,7 @@ from global_functions import plot_confusion_matrix, cm_analysis
 np.random.seed(7)
 
 # load the data
-file_name = 'data/bioresponse/bio_short.pkl'  # set working directory to MSc Project
+file_name = 'data/bioresponse/bio_clean.pkl'  # set working directory to MSc Project
 data = pd.read_pickle(file_name)
 
 # unbalanced data
@@ -26,12 +26,12 @@ X_train_unbalanced, X_test_unbalanced, y_train_unbalanced, y_test_unbalanced = t
 X_train_balanced, X_test_balanced, y_train_balanced, y_test_balanced = get_balanced_data(data)
 
 # unpack unbalanced model
-path = '1) classification algorithms/random forest/bioresponse/model_forest_unbalanced_bio_short.pkl'
+path = '1) classification algorithms/random forest/bioresponse/model_forest_unbalanced_bio.pkl'
 with open(path, 'rb') as file:
     unbalanced_model = pickle.load(file)
 
 # unpack balanced model
-path = '1) classification algorithms/random forest/bioresponse/model_forest_balanced_bio_short.pkl'
+path = '1) classification algorithms/random forest/bioresponse/model_forest_balanced_bio.pkl'
 with open(path, 'rb') as file:
     balanced_model = pickle.load(file)
 
@@ -43,14 +43,14 @@ balanced_predictions = [int(round(x)) for x in balanced_predictions]
 
 # print the confusion matrix, precision, recall, etc.
 get_model_performance(unbalanced_model, 'unbalanced', X_test_unbalanced, y_test_unbalanced, 'RF','bioresponse dataset')
-plt.savefig('1) classification algorithms/assess model performance/bioresponse/figures/PRcurve_rf_unbalanced_bio_short.png')
+plt.savefig('1) classification algorithms/assess model performance/bioresponse/figures/PRcurve_rf_unbalanced_bio.png')
 plt.close()
 get_model_performance(balanced_model, 'balanced', X_test_balanced, y_test_balanced,'RF','bioresponse dataset')
-plt.savefig('1) classification algorithms/assess model performance/bioresponse/figures/PRcurve_rf_balanced_bio_short.png')
+plt.savefig('1) classification algorithms/assess model performance/bioresponse/figures/PRcurve_rf_balanced_bio.png')
 plt.close()
 
-cm_analysis(y_test_balanced, balanced_predictions, filename='1) classification algorithms/assess model performance/bioresponse/figures/cm_rf_balanced_bio_short.png',labels=[0, 1],
-            ymap=['response','no response'],title='RF performance on balanced data\nbioresponse dataset')
+cm_analysis(y_test_balanced, balanced_predictions, filename='1) classification algorithms/assess model performance/bioresponse/figures/cm_rf_balanced_bio.png',labels=[0, 1],
+            ymap=['class0','class1'],title='RF performance on balanced data\nbioresponse dataset')
 
-cm_analysis(y_test_unbalanced,unbalanced_predictions,filename='1) classification algorithms/assess model performance/bioresponse/figures/cm_rf_unbalanced_bio_short.png',labels=[0, 1],
-            ymap=['response','no response'],title='RF performance on unbalanced data\nbioresponse dataset')
+cm_analysis(y_test_unbalanced,unbalanced_predictions,filename='1) classification algorithms/assess model performance/bioresponse/figures/cm_rf_unbalanced_bio.png',labels=[0, 1],
+            ymap=['class0','class1'],title='RF performance on unbalanced data\nbioresponse dataset')
