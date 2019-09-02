b320f002c530a502b9d190dab245e407771815bf
amarek1 <31806815+amarek1@users.noreply.github.com>
2019-09-02 15:38:21 +0100

    Create summary_figure.py

---
+++ 1) classification algorithms/assess model performance/credit card fraud/figures/summary_figure.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+plt.figure(1)
+plt.subplot(211)
+img1 = mpimg.imread('1) classification algorithms/assess model performance/credit card fraud/figures/cm_nn_balanced_fraud.png')
+plt.imshow(img1)
+
+plt.show()
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def show_images(images, cols=1, titles=None):
+    """Display a list of images in a single figure with matplotlib.
+
+    Parameters
+    ---------
+    images: List of np.arrays compatible with plt.imshow.
+
+    cols (Default = 1): Number of columns in figure (number of rows is
+                        set to np.ceil(n_images/float(cols))).
+
+    titles: List of titles corresponding to each image. Must have
+            the same length as titles.
+    """
+    assert ((titles is None) or (len(images) == len(titles)))
+    n_images = len(images)
+    if titles is None: titles = ['Image (%d)' % i for i in range(1, n_images + 1)]
+    fig = plt.figure()
+    for n, (image, title) in enumerate(zip(images, titles)):
+        a = fig.add_subplot(cols, np.ceil(n_images / float(cols)), n + 1)
+        if image.ndim == 2:
+            plt.gray()
+        plt.imshow(image)
+        a.set_title(title)
+    fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
+    plt.show()
+
+show_images([img1], cols=1, titles=None)
